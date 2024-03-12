1f345efa3d9c8df6f2eb4fb4f9c938faa92251d1
Ayokunnumi1 <omololuayk@gmail.com>
2024-03-12 18:07:26 +0100

    Update Cors orgin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://mentor-me-noww.vercel.app/"
+    origins "*"
 
     resource "*",
       headers: :any,
